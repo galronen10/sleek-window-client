73bbcad50d855299045fd505296577e3d7f895d2
galronen10 <galronen10@gmail.com>
2025-07-22 15:59:27 +0300

    delete store

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './components';
-import { store } from './store';
-import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </Provider>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
